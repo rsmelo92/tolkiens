28acd182fff2710710278c9e6be40e6480549c32
Rafael Melo <rsmelo_@hotmail.com>
2021-08-24 12:38:35 -0300

    chore: generate and serve tokens properly

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import getCss from 'get-css'
 import cssstats from 'cssstats'
+import fs from 'fs'
+import path from 'path'
+import archiver from 'archiver'
+import rimraf from 'rimraf'
 
 import { parseColor } from './parsers/color'
 import { buildDictionary } from './styleDictionary/build'
@@ -8,11 +12,32 @@ type ParseProperties = {
   css: string,
 }
 
+function zipFolder () {
+  const outputDir = path.join(__dirname, '/output.zip')
+  const buildDir = path.join(__dirname, 'build')
+  const output = fs.createWriteStream(outputDir)
+  const archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+  })
+
+  output.on('close', () => {
+    rimraf.sync(buildDir)
+  })
+
+  archive.pipe(output)
+  archive.directory('build/', false)
+  archive.glob('*', { cwd: path.join(__dirname, 'build') })
+  archive.finalize()
+}
+
 function parseProperties ({ css }: ParseProperties) {
   const { declarations: { properties } } = cssstats(css)
 
   // Colors
   parseColor(properties).forEach(buildDictionary)
+
+  // Zip all generated files
+  zipFolder()
 }
 
 async function fetchCode (url: string) {
