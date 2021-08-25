c6fb38d407d5226978efd9a973af3afd265f96ad
Rafael Melo <rsmelo_@hotmail.com>
2021-08-25 17:28:10 -0300

    chore: responsive

--- packages/client/src/components/Steps/index.tsx
+++ packages/client/src/components/Steps/index.tsx
@@ -6,17 +6,19 @@ function Steps() {
     <div className={styles.cardsWrapper}>
       <h2 className={styles.title}>Three Easy Steps</h2>
       <Card>
-        <div className={styles.cardInner}>
-          <h4 className={styles.cardNumber}>1</h4>
-          <p className={styles.text}>Fetch and analyze site</p>
-        </div>
-        <div className={styles.cardInner}>
-          <h4 className={styles.cardNumber}>2</h4>
-          <p className={styles.text}>Extract Design Tokens with statistical analysis</p>
-        </div>
-        <div className={styles.cardInner}>
-          <h4 className={styles.cardNumber}>3</h4>
-          <p className={styles.text}>Serve variations (Flutter, RN, Android, Sass...)</p>
+        <div className={styles.cardInnerWrapper}>
+          <div className={styles.cardInner}>
+            <h4 className={styles.cardNumber}>1</h4>
+            <p className={styles.text}>Fetch and analyze site</p>
+          </div>
+          <div className={styles.cardInner}>
+            <h4 className={styles.cardNumber}>2</h4>
+            <p className={styles.text}>Extract Design Tokens with statistical analysis</p>
+          </div>
+          <div className={styles.cardInner}>
+            <h4 className={styles.cardNumber}>3</h4>
+            <p className={styles.text}>Serve variations (Flutter, RN, Android, Sass...)</p>
+          </div>
         </div>
       </Card>
     </div>
